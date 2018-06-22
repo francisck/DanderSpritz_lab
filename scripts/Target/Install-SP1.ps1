71eb5039fd1b2128518ce911a9e05e3f15b530c3
Francisck <francisck@protonmail.ch>
2018-06-21 22:34:57 -0600

    removing local URL for Target SP1 install

--- scripts/Target/Install-SP1.ps1
+++ scripts/Target/Install-SP1.ps1
@@ -16,12 +16,10 @@ Set-StrictMode -Version Latest
 
 Try 
 {
-    (New-Object System.Net.WebClient).DownloadFile('http://172.28.128.3/KB976932-X64.exe', 'C:\Windows\Temp\KB976932-X64.exe')   
-    #(New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/0/A/F/0AFB5316-3062-494A-AB78-7FB0D4461357/windows6.1-KB976932-X64.exe', 'C:\Windows\Temp\KB976932-X64.exe')
+    (New-Object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/0/A/F/0AFB5316-3062-494A-AB78-7FB0D4461357/windows6.1-KB976932-X64.exe', 'C:\Windows\Temp\KB976932-X64.exe')
     Write-Host "Succesfully downloaded SP1. Starting installation. Hang on, this will take a long time"
     
     Start-Process -FilePath "C:\Windows\Temp\KB976932-X64.exe" -ArgumentList "/quiet", "/nodialog", "/norestart" -Wait
-    #cmd /c C:\Windows\Temp\KB976932-X64.exe /quiet /nodialog /norestart
 }
 Catch 
 {
